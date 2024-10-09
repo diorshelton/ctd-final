efae23eddcd9c337284977136e24078222d131e8
Dior.Shelton <dior.shelton@gmail.com>
2024-10-09 16:04:47 -0400

    add db connection

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
+require("dotenv").config()
 const express = require('express')
 const app = express()
 
+// CONNECT DB
+const connectDB = require("./db/connect")
 // Posts Router
 const postsRouter = require('./routes/posts')
 
 // middleware
 const notFound = require('./middleware/not-found')
 
-// app.get('/api/v1', (req, res) => {
-//   res.send("<h1/>Final Project</h1>")
-// })
-
-// app.get('/api/v1/:id', (req, res) => {
-//   console.log(req.params)
-//   res.send(`${req.params.id}`)
-// })
 
 // routes 
 app.use("/api/v1/posts", postsRouter)
@@ -23,4 +18,15 @@ app.use(notFound)
 
 const port =  process.env.PORT || 3000 
 
-app.listen(port, () =>  console.log(`listening on port ${port}`))
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}...`)
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+start();

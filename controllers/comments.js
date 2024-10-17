3fe66b7bcbd0855d7c501c8bb2900cbb6e32f513
Dior.Shelton <dior.shelton@gmail.com>
2024-10-16 20:26:39 -0400

    remove code comments and clean up error handling in comments.js controller

--- controllers/comments.js
+++ controllers/comments.js
@@ -4,17 +4,15 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const showComments = async (req, res) => {
-	try {
-		const postId = req.params;
-		// shows all comments on a single post
-		const comment = await Comment.where({ postId: postId.id });
-		res.status(StatusCodes.OK).json(comment);
-	} catch (err) {
-		console.log(err);
+	const postId = req.params;
+	// shows all comments on a single post
+	const comment = await Comment.where({ postId: postId.id });
+	if (!comment || comment.length === 0) {
+		return res.status(StatusCodes.OK).json({ message: "No comments yet" });
 	}
+	res.status(StatusCodes.OK).json(comment);
 };
-const addComment = async (req, res, next) => {
-	try {
+const addComment = async (req, res) => {
 		const {
 			body: { content },
 			user: { userId },
@@ -23,7 +21,7 @@ const addComment = async (req, res, next) => {
 		// Create comment
 		const comment = await Comment.create({ postId, userId, content });
 		// Get comment id
-		const commentId = await comment._id;
+		const commentId = comment._id;
 		// Push comment to post array
 		const post = await Post.findByIdAndUpdate(
 			{ _id: postId, createdBy: userId },
@@ -34,49 +32,41 @@ const addComment = async (req, res, next) => {
 			},
 			{ new: true, runValidators: true }
 		);
-		if (!comment) {
-			throw new Error(err.message);
-		}
 		if (!post || !postId) {
 			throw new NotFoundError(`No post found`);
 		}
-		// Return response with comment
-		// const postWithComment = await Comment.findById({commentId});
-	} catch (err) {
-		console.log(err);
-		res.send(err.message);
-	}
 	res.status(StatusCodes.OK).json({ msg: "Comment added" });
 };
 const removeComment = async (req, res) => {
 	const {
 		user: { userId },
-		params: { id: commentId },
+		headers: { c_id: commentId },
 	} = req;
 	// get commentId
-	// const commentId = "670ebc6405dfd1a58688f670";
 	const comment = await Comment.findByIdAndDelete({
 		_id: commentId,
 		createdBy: userId,
 	});
 	if (!comment) {
-		console.log(err);
 		throw new NotFoundError(`No comment with id ${commentId}`);
 	}
 	res.status(StatusCodes.OK).json({ msg: "The entry was deleted" });
 };
 const editComment = async (req, res) => {
 	const {
-		body: { content },
 		user: { userId },
-		params: { id: commentId },
+		headers: { c_id: commentId },
 	} = req;
 
 	const comment = await Comment.findByIdAndUpdate(
 		{ _id: commentId, createdBy: userId },
-		content,
+		req.body,
 		{ new: true, runValidators: true }
 	);
+	if (!comment) {
+		throw new NotFoundError(`No comment with id ${commentId}`);
+	}
+	res.status(StatusCodes.OK).json({ comment });
 };
 
 module.exports = { showComments, addComment, removeComment, editComment };
